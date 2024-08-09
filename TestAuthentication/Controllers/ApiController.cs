395e9585af36b9f62cb57979af7339dca4cda922
rajaduraiLearn <rajadurai.ir@gmail.com>
2024-08-09 10:14:40 -0700

    Updates with new changes

--- TestAuthentication/Controllers/ApiController.cs
+++ TestAuthentication/Controllers/ApiController.cs
@@ -1,5 +1,13 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
 using TestAuthentication.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using Microsoft.IdentityModel.Protocols;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System.Web.Management;
+using System.Security.Cryptography.Pkcs;
 
 namespace TestAuthentication.Controllers
 {
@@ -51,6 +59,94 @@ namespace TestAuthentication.Controllers
             // Implement your token validation logic here
             // This is just a placeholder for demonstration purposes
             return token == "your_valid_token";
+        }
+
+        [HttpPost]
+        public ActionResult VerifyPermission()
+        {
+            var authHeader = Request.Headers["Authorization"];
+
+            if (authHeader != null && authHeader.StartsWith("Bearer "))
+            {
+                var accessToken1 = authHeader.Substring("Bearer ".Length).Trim();
+
+                var tokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidAudiences = new[] { "api://7111800c-fba6-4c38-9364-aee0dcb19266", "audience2" }, // Add your valid audiences here
+                    ValidIssuers = new[] { "https://sts.windows.net/975f013f-7f24-47e8-a7d3-abc4752bf346/", "issuer2" }, // Add your valid issuers here
+                    IssuerSigningKeys = GetIssuerSigningKeys()
+                };
+
+                var tokenHandler = new JwtSecurityTokenHandler();
+                SecurityToken validatedToken;
+                try
+                {
+                    var principal = tokenHandler.ValidateToken(accessToken1, tokenValidationParameters, out validatedToken);
+
+                    // Extract the appid
+                    var appIdClaim = principal.Claims.FirstOrDefault(c => c.Type == "appid")?.Value;
+
+                    // List of allowed app IDs or object IDs
+                    var allowedIds = new List<string>
+                    {
+                        "b19896f1-d1af-4c0b-a976-11d4f3aab957",
+                        "AppId"
+                    };
+
+                    // Check if the token's appid or oid is in the list of allowed IDs
+                    if (allowedIds.Contains(appIdClaim))
+                    {
+                        // Token is valid and the managed identity is allowed
+                        var responseInfo = new
+                        {
+                            AppId = appIdClaim,
+                            Success = true
+                        };
+
+                        return Json(responseInfo);
+                    }
+                    else
+                    {
+                        var errorInfo = new
+                        {
+                            message = "appIdClaim is not valid"
+                        };
+
+                        return Json(errorInfo);
+                    }
+                }
+                catch (SecurityTokenValidationException ex)
+                {
+                    var errorInfo = new
+                    {
+                        message = $"Exception  {ex.Message}"
+                    };
+
+                    return Json(errorInfo);
+                }
+            }
+            else
+            {
+                var errorInfo = new
+                {
+                    message = "Auth Header not found"
+                };
+
+                return Json(errorInfo);
+            }
+        }
+
+        private IEnumerable<Microsoft.IdentityModel.Tokens.SecurityKey> GetIssuerSigningKeys()
+        {
+            var configurationManager = new ConfigurationManager<OpenIdConnectConfiguration>(
+                "https://login.microsoftonline.com/common/.well-known/openid-configuration",
+                new OpenIdConnectConfigurationRetriever());
+            var openIdConfig = configurationManager.GetConfigurationAsync().Result;
+            return openIdConfig.SigningKeys;
         }
     }
 }
